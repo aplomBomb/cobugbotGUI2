9f7f2b13729aba479e5b583da407a57e84165bad
aplombomb <seizethesteez@gmail.com>
2019-12-28 23:24:07 -0500

    lil tweaks, gettin too caught up in style | done for the night

--- src/views/Splash/Splash.js
+++ src/views/Splash/Splash.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Link as div } from "react-router-dom";
 import { Login } from "../Login/Login";
 
 export const Splash = () => {
   const [loginActive, setLoginActive] = useState(false);
-  const [loginClassName, setLoginClassName] = useState('Hidden' || 'Visible')
+  const [loginClassName, setLoginClassName] = useState("Hidden" || "Visible");
+  const [loginLinkState, setLoginLinkState] = useState('Active' || 'Inactive');
 
   useEffect(() => {
-    if (loginActive) setLoginClassName('Visible')
-      else setLoginClassName('Hidden')
-  } )
+    if (loginActive) {
+      setLoginClassName("Visible");
+      setLoginLinkState('Active');
+    } else {
+      setLoginClassName("Hidden");
+      setLoginLinkState('Inactive');
+    }
+  });
 
   return (
     <div className="splashContainer">
       <h1>Cobugbot</h1>
       <div className="linksContainer">
-        <div className="loginLink" onClick={() => setLoginActive(!loginActive)}>
+        <div
+          className={`loginLink${loginLinkState}`}
+          onClick={() => setLoginActive(!loginActive)}
+        >
           <h2>Login</h2>
         </div>
         <a className="aboutLink" href="https://discord.gg/PtXnfJm">
